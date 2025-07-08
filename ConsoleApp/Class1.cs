bddb74486d2681ac97b76900b279798c237a39f7
Rohan G K <ronney270422@gmail.com>
2025-07-08 11:04:33 +0530

    Add files via upload

---
+++ ConsoleApp/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ConsoleApp
+{
+    internal class Class1
+    {
+        public void Demo()
+        {
+            int voting = 18;
+            int candidate = 35;
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("For the given age voting ability is : " + (age >= voting));   //boolean
+
+            //condition statements
+            if (age >= voting && age >= candidate)
+            {
+                Console.WriteLine("Eligible for voting and as a candidate for election");
+            }
+            else if (age >= voting)
+            {
+                Console.WriteLine("Only eligible for voting in the election");
+            }
+
+        }
+
+    }
+}
