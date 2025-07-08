90b2ebd8af3f68158b0545ac9dec65e924da010d
Rohan G K <ronney270422@gmail.com>
2025-07-08 15:22:49 +0530

    New file Class4 for Method concepts

---
+++ ConsoleApp/Class4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+
+namespace ConsoleApp
+{
+    public class Class4  
+    {
+
+        //Methods and it's concepts
+        public void Method1()
+            {
+                Console.WriteLine("It is the 1st method example");
+            }
+
+        public static int Method2(int a, int b)
+        {
+            return a + b; 
+        }
+
+        public double Salary(double basicPay, int workingDays, double allowance = 1000, double bonus = 0)
+        {
+            return (basicPay / 30 * workingDays) + allowance + bonus; 
+        }
+
+        public double Salary(double basicPay, int workingDays)
+        {
+            return basicPay / 30 * workingDays;
+        }
+
+
+    }
+}
+
