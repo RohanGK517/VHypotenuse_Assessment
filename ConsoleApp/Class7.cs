a590b1096fa7307eb34cd1af5b50a59fb65a18c3
Rohan G K <ronney270422@gmail.com>
2025-07-10 14:23:21 +0530

    Class for Polymorphism and interface concepts

---
+++ ConsoleApp/Class7.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    //Polymorphism and Interface
+    public class Human   
+    {
+        public virtual void job()
+        {
+            Console.WriteLine("Human does their work");
+        }
+        public void salary()
+        {
+            Console.WriteLine("Only the teachers or trainers get the salary");
+        }
+    }
+    public class Students : Human
+    {
+        public override void job()
+        {
+            Console.WriteLine("Students have learn the concepts");
+        }
+    }
+
+    public class Teacher : Human
+    {
+        public override void job()
+        {
+            Console.WriteLine("Teacher train the students");
+        }
+    }
+
+    //Interface and Multiple Interface 
+
+    public interface IEat
+    {
+        void digest();
+    }
+    public interface IRun
+    {
+        void run();
+    }
+    public class Activity : IEat, IRun
+    {
+        public void run()
+        {
+            Console.WriteLine("Running is good for health");
+        }
+        public void digest()
+        {
+            Console.WriteLine("Eating food");
+        }
+    }
+}
